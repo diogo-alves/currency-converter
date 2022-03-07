9b4a4a41c6b0d3ea08bee553eeeeac487b145366
Diogo Alves <diogo.alves.ti@gmail.com>
2022-03-07 12:04:18 -0300

    Configura CORS
    Adiciona exemplo de configuraçao do CORS

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import api, exceptions
+from .config import settings
 
 app = FastAPI(
     title="Currency Converter",
     description="Uma API REST para conversão de moedas",
     version="1.0.0",
 )
+
+if settings.CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+    )
 
 
 @app.on_event('startup')
