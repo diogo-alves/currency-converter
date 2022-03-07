0eed477a34855aafb672511a720e50864a8bcbb5
Diogo Alves <diogo.alves.ti@gmail.com>
2022-03-07 15:31:56 -0300

    Adiciona informações extras à documentação da API
    Adiciona exemplo de resposta  HTTP 502

--- app/api/v1.py
+++ app/api/v1.py
@@ -1,15 +1,30 @@
 from decimal import Decimal
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, Query, status
 
 from ..enums import CurrencyCode
 from ..schemas import ConversionResponse
 from ..services import CurrencyConversionService
 
-router = APIRouter(prefix='/v1', tags=['Conversions'])
+router = APIRouter(prefix='/v1', tags=['Conversão'])
 
 
-@router.get("/conversions", response_model=ConversionResponse)
+@router.get(
+    '/conversion',
+    response_model=ConversionResponse,
+    summary='Utiliza parâmetros pré-definidos para realizar as conversões',
+    responses={
+        status.HTTP_502_BAD_GATEWAY: {
+            'content': {
+                'application/json': {
+                    'example': {
+                        'detail': 'Falha na comunicação com o servidor remoto.'
+                    }
+                }
+            },
+        }
+    },
+)
 def convert(
     from_currency: CurrencyCode = Query(..., alias='from'),
     to_currency: CurrencyCode = Query(..., alias='to'),
--- app/main.py
+++ app/main.py
@@ -5,9 +5,19 @@ from . import api, exceptions
 from .config import settings
 
 app = FastAPI(
-    title="Currency Converter",
-    description="Uma API REST para conversão de moedas",
+    title="Conversor de Moedas",
+    description="Uma API REST para conversão de moedas com base na cotação do dólar americano (USD)",  # noqa
     version="1.0.0",
+    license_info={
+        'name': 'Licença de uso',
+        'url': 'https://github.com/diogo-alves/currency-converter/blob/main/LICENSE',  # noqa
+    },
+    openapi_tags=[
+        {
+            'name': 'Conversão',
+            'description': 'Endpoint que realiza conversões entre moedas',
+        },
+    ],
 )
 
 if settings.CORS_ORIGINS:
