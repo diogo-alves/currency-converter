823ed933031774de13dc72b66ece1ad78587605e
Diogo Alves <diogo.alves.ti@gmail.com>
2022-03-07 10:44:44 -0300

    Move exception handler para o módulo exceptions

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,2 +1,23 @@
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+
+
 class APIConnectionError(Exception):
     ...
+
+
+def api_connection_exception_handler(
+    request: Request,
+    exc: APIConnectionError,
+):
+    return JSONResponse(
+        status_code=status.HTTP_502_BAD_GATEWAY,
+        content={'detail': 'Falha na comunicação com o servidor remoto.'},
+    )
+
+
+def register_exception_handler(app: FastAPI) -> None:
+    app.add_exception_handler(
+        APIConnectionError,
+        api_connection_exception_handler,
+    )
--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from decimal import Decimal
 
-from fastapi import Depends, FastAPI, Query, Request, status
-from fastapi.responses import JSONResponse
+from fastapi import Depends, FastAPI, Query
 
+from . import exceptions
 from .enums import CurrencyCode
-from .exceptions import APIConnectionError
 from .schemas import ConversionResponse
 from .services import CurrencyConversionService
 
@@ -13,17 +12,6 @@ app = FastAPI(
     description="Uma API REST para conversão de moedas",
     version="1.0.0",
 )
-
-
-@app.exception_handler(APIConnectionError)
-def api_connection_exception_handler(
-    request: Request,
-    exc: APIConnectionError,
-):
-    return JSONResponse(
-        status_code=status.HTTP_502_BAD_GATEWAY,
-        content={'detail': 'Falha na comunicação com o servidor remoto.'},
-    )
 
 
 @app.get("/conversions", response_model=ConversionResponse)
@@ -35,3 +23,8 @@ def convert(
 ) -> ConversionResponse:
     result = conversion_service.convert(from_currency, to_currency, amount)
     return ConversionResponse(result=result)
+
+
+@app.on_event('startup')
+def startup():
+    exceptions.register_exception_handler(app)
