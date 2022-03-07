ee8ab7ce87ba65adb006b8bd06c7137d9050fd50
Diogo Alves <diogo.alves.ti@gmail.com>
2022-03-06 23:43:25 -0300

    Adiciona arquivos de configuração

---
+++ config.py
@@ -0,0 +1,26 @@
+# from __future__ import annotations
+
+from functools import lru_cache
+from typing import List
+
+from pydantic import BaseSettings
+from pydantic.networks import AnyHttpUrl
+
+
+class Settings(BaseSettings):
+    EXCHANGE_RATE_API_KEY: str
+    EXCHANGE_RATE_API_URL: str
+    EXCHANGE_RATE_API_BASE: str
+    CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    class Config:
+        env_file = '.env'
+        case_sensitive = True
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
